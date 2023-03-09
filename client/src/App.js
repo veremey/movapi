e4c76096f5fbe84460e13a1b262ce401d82f4cc2
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-03-09 22:25:46 +0100

    added mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button'; 
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
+    <> 
+    <CssBaseline />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      Hello
+      <br />
+      <Button  variant="contained">Primary</Button>
+      <Button disabled>Disabled</Button>
+      <Button href="#text-buttons">Link</Button>
+      </div>
+  </>
   );
 }
 
