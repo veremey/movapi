9356fea56bc976204043917a27caeb171dfd98a4
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-03-11 21:21:49 +0100

    Added routers and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,21 @@
-import { AppBar, Box, Button, CssBaseline, Divider, Drawer, Hidden, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {Home, Settings} from './pages'
 
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
+import { CssBaseline } from '@mui/material';
+import { Navigation } from './components'
 import {useState} from 'react';
 
 function App() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const list = () => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-    >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-  
   return (
-    <> 
-    <CssBaseline />
-    <div className="App">
-      <Box>
-      <AppBar position="static">
-        <Toolbar>
-          <Hidden only={['lg', 'xl']}>
-          <IconButton
-            onClick={() => setIsDrawerOpen(true)}
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          </Hidden >
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-      anchor="left"
-      open={isDrawerOpen}
-      onClose={() => setIsDrawerOpen(false)}
-    >
-      {list()}
-    </Drawer>
-      
-    </Box>
-      </div>
-  </>
+    <BrowserRouter> 
+      <CssBaseline />
+      <Navigation />
+      <Routes >
+        <Route index element={<Home />} />
+        <Route path='settings' element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/navigation/index.js
@@ -0,0 +1,77 @@
+import { AppBar, Box, Button, Drawer, Hidden, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { Link } from 'react-router-dom'
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import {useState} from 'react';
+
+const Navigation = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+    >
+      <List>
+          <ListItem disablePadding>
+            <Link to='settings'>
+            <ListItemButton>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Settings"} />
+            </ListItemButton>
+            </Link>
+          </ListItem>
+      </List>
+    </Box>
+  );
+  
+  return (
+      <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Hidden only={['lg', 'xl']}>
+          <IconButton
+            onClick={() => setIsDrawerOpen(true)}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          </Hidden >
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to='settings'>
+              Movies recommendation
+            </Link>
+          </Typography>
+          <Box sx={{  display: { xs: 'none', lg: 'flex' } }}>
+              <Link to='settings'>
+                <Button
+                  sx={{ my: 2, color: 'white'}}
+                >
+                  Settings
+                </Button>
+              </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+      anchor="left"
+      open={isDrawerOpen}
+      onClose={() => setIsDrawerOpen(false)}
+    >
+      {list()}
+    </Drawer>
+    </Box>
+  );
+}
+
+export default Navigation;
