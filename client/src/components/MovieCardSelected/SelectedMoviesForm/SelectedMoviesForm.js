9d241943eb2bae5066121983c2d22d2e650e2d8d
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-03-20 21:39:40 +0100

    Add selected movie

--- client/src/components/MovieCardSelected/SelectedMoviesForm/SelectedMoviesForm.js
+++ client/src/components/MovieCardSelected/SelectedMoviesForm/SelectedMoviesForm.js
@@ -1,22 +1,43 @@
 import { Divider, IconButton, InputBase, Paper } from '@mui/material';
+import { Field, Form } from 'react-final-form';
 
 import CheckIcon from '@mui/icons-material/Check';
 
-const SelectedMoviesForm = () => (
-  <Paper
-    component="form"
-    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
-  >
-    <InputBase
-      sx={{ ml: 1, flex: 1 }}
-      placeholder="Put the list name"
-      inputProps={{ 'aria-label': 'put list name' }}
-    />
-    <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-    <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-      <CheckIcon />
-    </IconButton>
-  </Paper>
+const SelectedMoviesForm = ({ onSubmit }) => (
+  <Form
+    onSubmit={onSubmit}
+    validate={(values) => {
+      const errors = {};
+
+      if (!values.listName) {
+        errors.listName = 'Required';
+      }
+
+      return errors;
+    }}
+    render={({ handleSubmit }) => (
+      <form onSubmit={handleSubmit}>
+        <Paper
+          component="form"
+          sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+        >
+          <Field
+            name="listName"
+            render={({ input, meta }) => (
+              <>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Put the list name"
+                  inputProps={{ 'aria-label': 'put list name' }}
+                  {...input}
+                />
+              </>
+            )}
+          />
+        </Paper>
+      </form>
+    )}
+  />
 );
 
 export default SelectedMoviesForm;
