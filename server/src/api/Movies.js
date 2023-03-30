eddc9a4a86182f1e9ed9737172663707850cca19
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-03-30 12:30:35 +0200

    removed log

--- server/src/api/Movies.js
+++ server/src/api/Movies.js
@@ -4,7 +4,6 @@ const { Movies } = require('../entities/Movies');
 const { API_KEY, API_SOURCE} = require('../config');
 
 const getPopular = async (page, language) => {
-  console.log(language, ' <<<< lang');
   const result = await axios.get(`${API_SOURCE}/movie/popular?api_key=${API_KEY}&language=${language}&page=${page}`);
   
   return new Movies(result.data);
